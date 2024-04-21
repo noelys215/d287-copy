c3ca3ca0e900d0eb79e839a678743a45c45cde27
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-04-21 10:47:50 -0400

    update BootStrapData.java

--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -18,9 +18,6 @@ import java.util.Optional;
 
 /**
  *
- *
- *
- *
  */
 @Component
 public class BootStrapData implements CommandLineRunner {
@@ -33,45 +30,59 @@ public class BootStrapData implements CommandLineRunner {
     public BootStrapData(PartRepository partRepository, ProductRepository productRepository, OutsourcedPartRepository outsourcedPartRepository) {
         this.partRepository = partRepository;
         this.productRepository = productRepository;
-        this.outsourcedPartRepository=outsourcedPartRepository;
+        this.outsourcedPartRepository = outsourcedPartRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
-       /*
-        OutsourcedPart o= new OutsourcedPart();
-        o.setCompanyName("Western Governors University");
-        o.setName("out test");
-        o.setInv(5);
-        o.setPrice(20.0);
-        o.setId(100L);
-        outsourcedPartRepository.save(o);
-        OutsourcedPart thePart=null;
-        List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
-        for(OutsourcedPart part:outsourcedParts){
-            if(part.getName().equals("out test"))thePart=part;
+        if (partRepository.count() == 0 && productRepository.count() == 0) addSampleInventory();
+
+
+        List<OutsourcedPart> outsourcedParts = (List<OutsourcedPart>) outsourcedPartRepository.findAll();
+        for (OutsourcedPart part : outsourcedParts) {
+            System.out.println(part.getName() + " " + part.getCompanyName());
         }
 
-        System.out.println(thePart.getCompanyName());
-        */
-        List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
-        for(OutsourcedPart part:outsourcedParts){
-            System.out.println(part.getName()+" "+part.getCompanyName());
-        }
-
-        /*
-        Product bicycle= new Product("bicycle",100.0,15);
-        Product unicycle= new Product("unicycle",100.0,15);
-        productRepository.save(bicycle);
-        productRepository.save(unicycle);
-        */
 
         System.out.println("Started in Bootstrap");
-        System.out.println("Number of Products"+productRepository.count());
+        System.out.println("Number of Products" + productRepository.count());
         System.out.println(productRepository.findAll());
-        System.out.println("Number of Parts"+partRepository.count());
+        System.out.println("Number of Parts" + partRepository.count());
         System.out.println(partRepository.findAll());
 
+    }
+
+    private void addSampleInventory() {
+        /* Add sample parts */
+        createAndSaveOutsourcedPart("Neck", 50.0, 10, "Neck Supplier Co.");
+        createAndSaveOutsourcedPart("Fretboard", 30.0, 20, "Fretboard Supplies Ltd.");
+        createAndSaveOutsourcedPart("Body", 100.0, 15, "Guitar Bodies Inc.");
+        createAndSaveOutsourcedPart("Pickup", 75.0, 25, "Pickups International");
+        createAndSaveOutsourcedPart("Tuning Machine", 45.0, 30, "Tuners LLC");
+
+        /* Add sample products */
+        createAndSaveProduct("Stratocaster", 700.0, 5);
+        createAndSaveProduct("Telecaster", 650.0, 5);
+        createAndSaveProduct("Les Paul", 800.0, 5);
+        createAndSaveProduct("SG", 750.0, 5);
+        createAndSaveProduct("Jazzmaster", 720.0, 5);
+    }
+
+    private void createAndSaveOutsourcedPart(String name, double price, int inventory, String companyName) {
+        OutsourcedPart part = new OutsourcedPart();
+        part.setName(name);
+        part.setPrice(price);
+        part.setInv(inventory);
+        part.setCompanyName(companyName);
+        outsourcedPartRepository.save(part);
+    }
+
+    private void createAndSaveProduct(String name, double price, int inventory) {
+        Product product = new Product();
+        product.setName(name);
+        product.setPrice(price);
+        product.setInv(inventory);
+        productRepository.save(product);
     }
 }
